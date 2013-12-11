c197c0369450b69ed954752699e573d5e52fe9bf
Lujiajing <lujiajing1126@gmail.com>
2013-12-11 21:20:11 +0800

    Update config.php

--- App/Home/Conf/config.php
+++ App/Home/Conf/config.php
@@ -5,7 +5,7 @@ return array(
         'DB_HOST'   => 'localhost', // 服务器地址
         'DB_NAME'   => 'awc-jp', // 数据库名
         'DB_USER'   => 'root', // 用户名
-        'DB_PWD'    => '123456', // 密码
+        'DB_PWD'    => '#password', // 密码
         'DB_PORT'   => 3306, // 端口
         'DB_PREFIX' => '', // 数据库表前缀
 		'LAYOUT_ON' => false,
